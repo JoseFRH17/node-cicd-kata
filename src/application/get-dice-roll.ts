f8eba8b785495423cb7f2c815108d717042783ef
Jose Fco Rguez Hdez <josefrodriguezh@gmail.com>
2026-09-14 12:57:05 +0100

    feat: add linter error

--- src/application/get-dice-roll.ts
+++ src/application/get-dice-roll.ts
@@ -1,6 +1,6 @@
 export function getDiceRoll(random: () => number = Math.random): number {
   const lintDemo = 'unused on purpose';
-  const lintAny: any = 0;
+  const lintAny:any=0;
   if (lintAny === 1) return 1;
   return Math.floor(random() * 6) + 1;
 }
