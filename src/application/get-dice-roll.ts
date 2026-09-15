a4893b7f9430d5d842ee983c206f93b200a378da
Jose Fco Rguez Hdez <josefrodriguezh@gmail.com>
2026-09-15 19:28:00 +0100

    fix: errors

--- src/application/get-dice-roll.ts
+++ src/application/get-dice-roll.ts
@@ -1,6 +1,3 @@
 export function getDiceRoll(random: () => number = Math.random): number {
-  const lintDemo = 'unused on purpose';
-  const lintAny: any = 0;
-  if (lintAny === 1) return 1;
   return Math.floor(random() * 6) + 1;
 }
