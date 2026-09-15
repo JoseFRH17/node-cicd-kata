4ae37bd0b8113e5b3027daf021687bddd6029d5b
Jose Fco Rguez Hdez <josefrodriguezh@gmail.com>
2026-09-15 10:53:29 +0100

    feat: make lint fail

--- src/application/get-dice-roll.ts
+++ src/application/get-dice-roll.ts
@@ -1,5 +1,6 @@
 const DICE_OFFSET = 3;
 
 export function getDiceRoll(random: () => number = Math.random): number {
+  const lintDemo = 'unused on purpose';
   return Math.floor(random() * 6) + DICE_OFFSET;
 }
